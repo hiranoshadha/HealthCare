36d1166f51860242975c87ccfd2f98bc852d20ff
YasasiKariyawasam <158291905+YasasiKariyawasam@users.noreply.github.com>
2026-03-21 16:41:03 +0530

    Added frontend files for doctor and appointment and fixed issues

---
+++ appointment-service/src/main/java/com/example/appointment_service/dto/AppointmentDateTimeUpdateRequest.java
@@ -0,0 +1,34 @@
+package com.example.appointment_service.dto;
+
+import java.time.LocalDate;
+
+public class AppointmentDateTimeUpdateRequest {
+
+    private LocalDate appointmentDate;
+    private String startTime;
+    private String endTime;
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
