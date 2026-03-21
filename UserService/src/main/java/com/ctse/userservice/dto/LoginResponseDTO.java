4cdb54e3eb427f8689392874629bd8946b3c7f1f
Ishan Madusanka <madusankaishan13@gmail.com>
2026-03-21 10:07:31 +0530

    User Service FE implemented

---
+++ UserService/src/main/java/com/ctse/userservice/dto/LoginResponseDTO.java
@@ -0,0 +1,14 @@
+package com.ctse.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String token;
+    private Object user;
+    private String role;
+}
