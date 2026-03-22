16bebe6c886620ed0252601feb1a600d1b21ce3f
Hiran Oshadha <128114334+hiranoshadha@users.noreply.github.com>
2026-03-22 15:00:00 +0000

    add doctor display part

---
+++ HospitalService/src/main/java/com/ctse/hospitalservice/dto/DoctorScheduleDTO.java
@@ -0,0 +1,22 @@
+package com.ctse.hospitalservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO for schedule data received from Doctor Service.
+ * Mirrors the DoctorSchedule model in Doctor Service.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorScheduleDTO {
+    private Long scheduleId;
+    private Long doctorId;
+    private Long hospitalId;
+    private String dayOfWeek;
+    private String startTime;
+    private String endTime;
+    private int slotDuration;
+}
